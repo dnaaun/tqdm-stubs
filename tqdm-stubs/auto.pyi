dcfedfda7b52f8a329af8dca66376f788b0ff3b3
David Assefa Tofu <davidat@bu.edu>
2020-08-05 04:26:45 -0400

    add auto.pyi

--- tqdm-stubs/auto.pyi
+++ tqdm-stubs/auto.pyi
@@ -1,1 +1,15 @@
-from .autonotebook import tqdm as tqdm, trange as trange
+import sys
+import os
+
+try:
+    get_ipython = sys.modules["IPython"].get_ipython
+    if "IPKernelApp" not in get_ipython().config:
+        raise ImportError("console")
+    if "VSCODE_PID" in os.environ:
+        raise ImportError("vscode")
+except Exception:
+    from .std import tqdm, trange
+else:
+    from .notebook import tqdm, trange
+
+__all__ = ["tqdm", "trange"]
